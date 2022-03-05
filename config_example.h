125a82cfcf24ce6761aa6afd75d590374df6ecc0
Roberto Paes <robertocpaes@gmail.com>
2022-03-05 20:16:51 -0300

    init repository

---
+++ config_example.h
@@ -0,0 +1,9 @@
+#define WIFI_SSID ""    
+#define WIFI_PASS ""
+const char* mqttserver = "yourhomeassistantip";
+const int mqttserverport = 1883;
+const char* mqttuser = "yourhomeassistantuser";
+const char* mqttpass = "yourhomeassistantpass";
+const char* MQTTQUEUEINPUT = "inTopic";
+const char* MQTTQUEUEOUTPUT = "outTopic";
+long interval = 5000;
